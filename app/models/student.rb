56d6942ee4f99f1c8ef89da32f87efa0f2648740
Luis Mendoza <luis@mendoza.io>
2016-01-27 10:41:19 -0600

    Updated home page and set validations in student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ActiveRecord::Base
+  validates :email, :clave_unica, :points, presence: true
+  validates :email, :clave_unica, uniqueness: true
   def self.search(query)
     where("clave_unica = ?", "#{query}")
   end
